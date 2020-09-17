76f99a74a42ac22778b9db8b2d1697741549cbdc
prince210 <53873186+prince210@users.noreply.github.com>
2020-09-18 01:18:42 +0530

    Update count number of ways to reach a given score.java

--- Dynamic Programming/count number of ways to reach a given score.java
+++ Dynamic Programming/count number of ways to reach a given score.java
@@ -10,6 +10,9 @@ public class countWaysToReachScore
 	int[] ways = {3,5,10};
 	int count = countWays(n,dp,ways,0);
 	System.out.println(count);
+
+    	// method 2 
+	countWaysUsingTabulation(n);
     }
 
     public static int countWays(int n,Integer [][]dp,int[] ways,int i)
@@ -31,5 +34,29 @@ public class countWaysToReachScore
 	int count = 0;
 	count += countWays(n-ways[i],dp,ways,i)+countWays(n,dp,ways,i+1);
 	return dp[i][n] = count;
+    }
+	
+    // method - 2
+    public static void countWaysUsingTabulation(int n)
+    {
+	 int[] dp = new int[n+1];
+	 dp[0] = 1; // if n == 0 we can always archive this score without doing anything
+	    
+	 for(int i = 3;i <= n;i++)
+	 {
+	      dp[i] += dp[i-3];
+	 }
+	 
+	 for(int i = 5;i <= n;i++)
+	 {
+	      dp[i] += dp[i-5];
+	 }
+	 
+	 for(int i = 10;i <= n;i++)
+	 {
+	      dp[i] += dp[i-10];
+	 }
+	 
+	 System.out.println(dp[n]);
     }
 }
