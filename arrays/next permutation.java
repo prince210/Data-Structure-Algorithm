e7a4b904cd939ab6f5f49c93c7f01067ecf96f0d
Prince Singh <53873186+prince210@users.noreply.github.com>
2021-02-15 23:39:06 +0530

    next permutation

---
+++ arrays/next permutation.java
@@ -0,0 +1,43 @@
+public class Solution {
+    public int[] nextPermutation(int[] A) 
+    {
+        int n = A.length;
+        int i = n - 2;
+        
+        while(i >= 0 && A[i] >= A[i+1])
+        {
+            i--;
+        }
+        
+        if(i >= 0)
+        {
+            int j = n-1;
+            while(A[j] <= A[i])
+            {
+                j--;
+            }
+            swap(A,i,j);
+        }
+        
+        reverse(A,i+1,n-1);
+        
+        return A;
+    }
+    
+    public void reverse(int []arr,int i,int j)
+    {
+        while(i < j)
+        {
+            swap(arr,i++,j--);
+        }
+    }
+    
+    public void swap(int[] arr,int i,int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    // [2,1,3] --> [2,3,1]
+}
